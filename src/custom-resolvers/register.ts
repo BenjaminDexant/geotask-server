028356efeec09917987e01d004955d9eae0c6707
benjamin <benjamin.d@make-it.fr>
2022-03-05 18:41:25 +0100

    update and create User and Task collections models & and update resolver

--- src/custom-resolvers/register.ts
+++ src/custom-resolvers/register.ts
@@ -12,16 +12,16 @@ class RegisterResolver {
   async register(
     @Ctx() ctx: { prisma: PrismaClient },
     @Arg('data', () => RegisterInput)
-    { email, firstname, lastname, password }: RegisterInput
+    { email, password }: RegisterInput
   ): Promise<User> {
     const hashedPassword = await bcrypt.hash(password, 12);
     const user = await ctx.prisma.user.create({
       data: {
-        firstname,
-        lastname,
         email,
         password: hashedPassword,
         role: 'USER',
+        updatedAt: new Date(),
+        createdAt: new Date(),
       },
     });
 
